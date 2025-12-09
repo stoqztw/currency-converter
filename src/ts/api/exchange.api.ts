4ffd5e59d5892d77306748e749f6465bf7efde0b
atsq <ap3994741@gmail.com>
2025-12-09 17:06:03 +0300

    add api key

---
+++ src/ts/api/exchange.api.ts
@@ -0,0 +1,12 @@
+import type { ExchangeConfig } from "../types/exchange.types";
+
+const exchangeConfig: ExchangeConfig = {
+	API_KEY: "338a2d15e912b2621a3afd88",
+};
+
+// export let urlRequest = `https://v6.exchangerate-api.com/v6/${exchangeConfig.API_KEY}/latest/`;
+export let urlRequest = `remove me`;
+
+// export let apiResponce = getRecurce(
+// 	`https://v6.exchangerate-api.com/v6/${exchangeConfig.API_KEY}/latest/`
+// );
